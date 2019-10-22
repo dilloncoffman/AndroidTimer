26517fb002c0a7a1a0e05c1e4b1e53369bff92db
Dillon Coffman <tug40394@temple.edu>
2019-10-22 16:15:00 -0400

    Anonymous thread for countdown

--- app/src/main/java/edu/temple/timer_10_22_19/MainActivity.java
+++ app/src/main/java/edu/temple/timer_10_22_19/MainActivity.java
@@ -11,8 +11,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        for (int i = 20; i >= 0; i--) {
-            Log.d("Countdown", String.valueOf(i));
-        }
+        // Create an Anonymous Thread class, since it's single use, don't need to go through naming the class
+        new Thread () {
+            @Override
+            public void run() {
+                for (int i = 20; i >= 0; i--){
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e){
+                        e.printStackTrace();
+                    }
+                    Log.d("Countdown",String.valueOf(i));
+                }
+            }
+        }.start(); // could also just say Thread t = new Thread() and then call t.start() to have a reference to that thread to do something else later
     }
 }
