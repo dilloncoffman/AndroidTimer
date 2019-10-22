504aa54dddb5aa3226dc97766e36770c28f04c74
Dillon Coffman <tug40394@temple.edu>
2019-10-22 16:27:57 -0400

    First version of timer using Main thread's Handler and using Message passing

--- app/src/main/java/edu/temple/timer_10_22_19/MainActivity.java
+++ app/src/main/java/edu/temple/timer_10_22_19/MainActivity.java
@@ -1,5 +1,7 @@
 package edu.temple.timer_10_22_19;
 
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,6 +11,14 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     TextView timerLabel;
+
+    // always import android.os.Handler, not the .util one
+    Handler timerHandler = new Handler(new Handler.Callback() {
+        @Override
+        public boolean handleMessage(Message msg) { // callback invoked every time a message is received from a worker thread
+            return false;
+        }
+    }); // use a Handler that takes a callback object
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,8 +40,11 @@ public class MainActivity extends AppCompatActivity {
                             } catch (InterruptedException e){
                                 e.printStackTrace();
                             }
-                            //Log.d("Countdown",String.valueOf(i));
-                            timerLabel.setText(String.valueOf(i));
+                            // Ask the Main thread via its Handler to do the job for us of counting down
+                            // 1. Need a Message object
+                            Message msg = new Message(); // allows you to pass messages, stuff something into Message object and pass it to the Main thread via a Handler
+                            msg.what = i; // .what takes a single integer value
+                            timerHandler.sendMessage(msg); // use Main thread's Handler; Worker thread can access any object as long as it's not a View
                         }
                     }
                 }.start(); // could also just say Thread t = new Thread() and then call t.start() to have a reference to that thread to do something else later
