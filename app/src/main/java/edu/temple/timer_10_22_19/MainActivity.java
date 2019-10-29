d906801b8103fc442aec29dd51024ece3fd671c0
Dillon Coffman <tug40394@temple.edu>
2019-10-29 16:20:45 -0400

    Button states using enums

--- app/src/main/java/edu/temple/timer_10_22_19/MainActivity.java
+++ app/src/main/java/edu/temple/timer_10_22_19/MainActivity.java
@@ -4,13 +4,16 @@ import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView timerLabel;
+    Button button;
+
+    TimerStates timerState = TimerStates.STOPPED;
 
     // always import android.os.Handler, not the .util one
     Handler timerHandler = new Handler(new Handler.Callback() {
@@ -27,26 +30,41 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         timerLabel = findViewById(R.id.timerLabel);
+        button = findViewById(R.id.button);
 
-        findViewById((R.id.startButton)).setOnClickListener(new View.OnClickListener() {
+
+        // Create an Anonymous Thread class, since it's single use, don't need to go through naming the class
+        Thread t = new Thread () {
             @Override
-            public void onClick(View v) {
-                // Create an Anonymous Thread class, since it's single use, don't need to go through naming the class
-                new Thread () {
-                    @Override
-                    public void run() {
-                        for (int i = 20; i >= 0; i--){
-                            try {
-                                Thread.sleep(1000);
-                            } catch (InterruptedException e){
-                                e.printStackTrace();
-                            }
-                            // Ask the Main thread via its Handler to do the job for us of counting down
-                            timerHandler.sendEmptyMessage(i); // if all you want to send is one integer value
-                        }
+            public void run() {
+                for (int i = 20; i >= 0; i--){
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e){
+                        e.printStackTrace();
                     }
-                }.start(); // could also just say Thread t = new Thread() and then call t.start() to have a reference to that thread to do something else later
+                    // Ask the Main thread via its Handler to do the job for us of counting down
+                    timerHandler.sendEmptyMessage(i); // if all you want to send is one integer value
+                }
+            }
+        };
+
+        button.setOnClickListener(v -> { // lambda function, v is the View object on which the event is occurring
+            // Change behavior of button to move through various states, button can pause and start, not stop, timer will only stop when time runs out
+            if (timerState == TimerStates.STOPPED) {
+                timerState = TimerStates.RUNNING;
+                button.setText("Pause");
+            } else if (timerState == TimerStates.RUNNING) {
+                timerState = TimerStates.PAUSED;
+                button.setText("Start");
+            } else {
+                // Timer was PAUSED, but clicked Start
+                timerState = TimerStates.RUNNING;
+                button.setText("Pause");
             }
         });
     }
+
+    enum TimerStates {STOPPED, RUNNING, PAUSED}
+
 }
